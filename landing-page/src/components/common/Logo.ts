46971bb4cd96d7aca4b375f435be72de1b4453a2
sonu <instaheri6@gmail.com>
2026-08-05 16:14:21 +0530

    feat: update Logo URL, refactor TopBar component for improved layout, and enhance main.tsx structure

--- landing-page/src/components/common/Logo.ts
+++ landing-page/src/components/common/Logo.ts
@@ -1,2 +1,2 @@
 
-export const Logo:string = "src/assets/Nohma_logo_split_1-2-59_e3z182.png"
+export const Logo:string = "https://res.cloudinary.com/dakuzl3ve/image/upload/v1785926080/Nohma_logo_split_1-2-59_e3z182_gxhtrx.png"
--- landing-page/src/components/navigation/TopBar.tsx
+++ landing-page/src/components/navigation/TopBar.tsx
@@ -8,22 +8,22 @@ function TopBar({ productMenu, utilityMenu }: TopBarProps) {
   const { setActiveTabIndex, activeTabIndex, setActiveSubMenuIndex } = useNavigationContext();
   return (
     <div
-      className="flex h-full w-screen flex-col text-sm md:h-9 md:w-full md:flex-row md:items-center md:justify-center lg:items-center lg:gap-10"
+      className="flex h-full w-full flex-col text-sm md:h-9 md:w-full md:flex-row md:items-center md:justify-center lg:items-center lg:gap-10"
       onMouseEnter={() => setActiveSubMenuIndex(-1)}
     >
-      <nav className="flex h-full w-full flex-col gap-5 pt-5 pb-5 md:ml-52 md:flex-row md:items-center md:justify-start md:gap-0 md:pt-0 md:pb-0">
+      <nav className="flex h-full w-full flex-col gap-5 pt-5 pb-5 lg:ml-52 md:flex-row md:items-center md:justify-start md:gap-0 md:pt-0 md:pb-0">
         {productMenu.map((item, index) => (
           <li
             key={item.name}
-            className={`bl-1 flex h-full w-full list-none items-center pr-8 pl-8 text-center font-medium hover:font-bold md:border-l md:border-gray-300 ${index == productMenu.length - 1 ? "md:border-r" : ""} ${activeTabIndex == index ? "md:border-t-2 md:border-orange-500 md:bg-white md:font-bold" : "md:font-extralight"} `}
+            className={`bl-1 flex h-full w-full list-none items-center pr-8 pl-8 justify-center font-medium hover:font-bold md:border-l md:border-gray-300 ${index == productMenu.length - 1 ? "md:border-r" : ""} ${activeTabIndex == index ? "md:border-t-2 md:border-orange-500 md:bg-white md:font-bold" : "md:font-extralight"} `}
             onClick={() => setActiveTabIndex(index)}
           >
             <a
-              className="flex items-center gap-1 whitespace-nowrap lg:block lg:gap-0"
+              className="flex items-center gap-1 whitespace-nowrap lg:flex lg:flex-col lg:gap-0"
               href={item.link}
             >
               {item.name.split(" ").map((word, wordIndex) => (
-                <span key={wordIndex} className="block">
+                <span key={wordIndex} className="block h-4">
                   {word}
                   <br className="hidden lg:block" />
                 </span>
